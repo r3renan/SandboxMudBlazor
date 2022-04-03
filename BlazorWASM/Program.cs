59cfd4224971253066f984af1ee7b96330b38470
Renan Ramos <renan.ramos@vibetecnologia.com>
2022-04-03 18:23:59 -0300

    Mudança de nome da solução e do projeto.

--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -1,4 +1,4 @@
-using BlazorWebAssemblyAppTeste;
+using BlazorWASM;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
